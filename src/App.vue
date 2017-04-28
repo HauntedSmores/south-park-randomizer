0a6795a3d64620e874c9abd873d2808c3c9514a2
Darren Segal <me@darren-segal.com>
2017-04-28 02:20:06 -0400

    Random episode button

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 		  <div class="row">
 			<div class="col-md-12">
 				<h1>South Park Randomizer</h1>
-				<p>{{ randomNumber }}</p>
+				<p>{{ randomEpisode }}</p>
 				<button v-on:click="generate()">Generate Episode</button>
 			</div>
 		  </div>
@@ -17,13 +17,12 @@ import * as axios from 'axios';
 export default {
 		name: 'app',
 		created () {
-			const key = 'api_key=11067bbd9f322ba00d3797f02bca8c91f4f41154';
 			var http = axios.create({baseURL: 'http://api-public.guidebox.com/v2/'});
 
-			http.get('shows/405/episodes?season=1&' + key)
+			http.get('shows/405/seasons?' + this.key)
 			.then(response => {
 				console.log(response);
-				this.data = response.data;
+				this.total_seasons = response.data.total_results;
 			})
 			.catch(function (error) {
 				console.log(error);
@@ -32,14 +31,34 @@ export default {
 		data () {
 			return {
 				data: {},
-				randomNumber: "No random number."
+				key: 'api_key=11067bbd9f322ba00d3797f02bca8c91f4f41154',
+				total_seasons: "",
+				total_episodes: "",
+				season: "",
+				episode: "",
+				randomEpisode: "No random number."
 
 			}
 		},
 		methods: {
 			generate: function() {
-				this.randomNumber = Math.floor(Math.random() * (13 - 1)) + 1;
-				console.log(this.randomNumber);
+				var http = axios.create({baseURL: 'http://api-public.guidebox.com/v2/'});
+				let season = Math.floor(Math.random() * (this.total_seasons - 1)) + 1;
+				let episode;
+				http.get('shows/405/episodes?season=' + season + '&' + this.key)
+				.then(response => {
+					this.total_episodes = response.data.total_results;
+					console.log(this.total_episodes);
+					episode = Math.floor(Math.random() * (this.total_episodes - 1)) + 1;
+					this.randomEpisode = "Season " + season + " Episode " + episode;
+					this.season = season;
+					this.episode = episode;
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+			},
+			getEpisode: function() {
 			}
 		}
 	}
